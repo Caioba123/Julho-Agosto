fc4cad4812b05f205511a17bc02db6ffb4205752
Caio Müller <caiomuller7192@gmail.com>
2026-08-14 08:33:23 -0300

    Add files via upload

---
+++ sistema_matriculas/bin/sistema_matriculas.dart
@@ -0,0 +1,5 @@
+import 'package:sistema_matriculas/sistema_matriculas.dart' as sistema_matriculas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sistema_matriculas.calculate()}!');
+}
