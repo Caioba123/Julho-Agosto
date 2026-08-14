6b162e0fddaa34d7b4c6602029df9ed14f360bba
Caio Müller <caiomuller7192@gmail.com>
2026-08-14 10:37:54 -0300

    Delete sistema_matriculas directory

--- sistema_matriculas/bin/sistema_matriculas.dart
+++
@@ -1,5 +0,0 @@
-import 'package:sistema_matriculas/sistema_matriculas.dart' as sistema_matriculas;
-
-void main(List<String> arguments) {
-  print('Hello world: ${sistema_matriculas.calculate()}!');
-}
